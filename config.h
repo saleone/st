4ae707148384523654ed31d5905b03b32a6e85b9
Saša Savić <sasa.savic@pm.me>
2019-02-02 01:08:17 +0100

    Reduce pallete to minimum non monochome colors

--- config.h
+++ config.h
@@ -85,30 +85,30 @@ unsigned int tabspaces = 4;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
+	"#000000",
 	"red3",
 	"green3",
 	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#eeeeee",
+	"#eeeeee",
+	"#eeeeee",
+	"#eeeeee",
 
 	/* 8 bright colors */
-	"gray50",
+	"#eeeeee",
 	"red",
 	"green",
 	"yellow",
 	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#ffffff",
+	"#ffffff",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#eeeeee",
+	"#ffffff",
 };
 
 
